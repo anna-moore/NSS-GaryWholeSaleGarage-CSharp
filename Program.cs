161c76314c75d5d7907d38c8cf3bf07d11b101f4
anna-moore <ammoore434@gmail.com>
2021-03-12 13:56:10 -0600

    instance of each vehicle

--- Program.cs
+++ Program.cs
@@ -6,7 +6,33 @@ namespace GarysWholesaleGarage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Zero moto = new Zero()
+            {
+                BatteryKWh = 2000,
+                MainColor = "white",
+                MaximumOccupancy = "2"
+            };
+
+            Cessna Ces = new Cessna()
+            {
+                FuelCapacity = 3600,
+                MainColor = "gray",
+                MaximumOccupancy = "10"
+            };
+
+            Tesla tes = new Tesla()
+            {
+                BatteryKWh = 2300,
+                MainColor = "black",
+                MaximumOccupancy = "4"
+            };
+
+            Ram ra = new Ram()
+            {
+                FuelCapacity = 340,
+                MainColor = "sky blue",
+                MaximumOccupancy = "5"
+            };
         }
     }
 }
